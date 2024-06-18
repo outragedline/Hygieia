0f79ed76521181414435f6a8592be0f2215150cf
outragedline <outragedline@proton.me>
2024-06-18 01:21:22 -0300

    Mudar ordem de funcoes para ficar de acordo com o padrao

--- src/db.c
+++ src/db.c
@@ -505,43 +505,6 @@ void freeAgendamentoLista(AgendamentoLista *lista)
 	free(lista);
 }
 
-int inserirMedico(Medico *medico)
-{
-	sqlite3 *db;
-	sqlite3_stmt *stmt;
-	int rc = sqlite3_open(DB_STRING, &db);
-	if (rc != SQLITE_OK) {
-		fprintf(stderr, "Erro %d: %s\n", rc, sqlite3_errmsg(db));
-		return ERROR_CODE;
-	}
-
-	char *sql = "INSERT INTO Medico(nome,especialidade,cod) "
-		    "VALUES(:nome,:especialidade,:cod)";
-	rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
-	rc = sqlite3_bind_text(stmt, 1, medico->nome, -1, SQLITE_TRANSIENT);
-	rc = sqlite3_bind_text(stmt, 2, medico->especialidade, -1,
-			       SQLITE_TRANSIENT);
-	rc = sqlite3_bind_int(stmt, 3, medico->cod);
-
-	rc = sqlite3_step(stmt);
-	if (rc != SQLITE_OK && rc != SQLITE_DONE) {
-		fprintf(stderr, "Erro %d: %s\n", rc, sqlite3_errmsg(db));
-		sqlite3_close(db);
-		return ERROR_CODE;
-	}
-
-	rc = sqlite3_finalize(stmt);
-	if (rc != SQLITE_OK) {
-		fprintf(stderr, "Erro %d: %s\n", rc, sqlite3_errmsg(db));
-		sqlite3_close(db);
-		return ERROR_CODE;
-	}
-
-	medico->id = sqlite3_last_insert_rowid(db);
-	sqlite3_close(db);
-	return OK_CODE;
-}
-
 int inserirPaciente(Paciente *paciente)
 {
 	sqlite3 *db;
@@ -585,6 +548,43 @@ int inserirPaciente(Paciente *paciente)
 	}
 
 	paciente->id = sqlite3_last_insert_rowid(db);
+	sqlite3_close(db);
+	return OK_CODE;
+}
+
+int inserirMedico(Medico *medico)
+{
+	sqlite3 *db;
+	sqlite3_stmt *stmt;
+	int rc = sqlite3_open(DB_STRING, &db);
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "Erro %d: %s\n", rc, sqlite3_errmsg(db));
+		return ERROR_CODE;
+	}
+
+	char *sql = "INSERT INTO Medico(nome,especialidade,cod) "
+		    "VALUES(:nome,:especialidade,:cod)";
+	rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+	rc = sqlite3_bind_text(stmt, 1, medico->nome, -1, SQLITE_TRANSIENT);
+	rc = sqlite3_bind_text(stmt, 2, medico->especialidade, -1,
+			       SQLITE_TRANSIENT);
+	rc = sqlite3_bind_int(stmt, 3, medico->cod);
+
+	rc = sqlite3_step(stmt);
+	if (rc != SQLITE_OK && rc != SQLITE_DONE) {
+		fprintf(stderr, "Erro %d: %s\n", rc, sqlite3_errmsg(db));
+		sqlite3_close(db);
+		return ERROR_CODE;
+	}
+
+	rc = sqlite3_finalize(stmt);
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "Erro %d: %s\n", rc, sqlite3_errmsg(db));
+		sqlite3_close(db);
+		return ERROR_CODE;
+	}
+
+	medico->id = sqlite3_last_insert_rowid(db);
 	sqlite3_close(db);
 	return OK_CODE;
 }
